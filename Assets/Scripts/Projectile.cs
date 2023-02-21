adb025b1b20fc808cd593456d8e176aa3343dfb5
Brian Guerrero <brian.guerrero0210@gmail.com>
2023-02-21 14:52:29 -0500

    shooting paint ball and projectile script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter()
+	{
+        Destroy(gameObject);
+	}
+}
