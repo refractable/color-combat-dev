8d54c52c9211b603820718457a3a63ea72ca7cdc
Brian Guerrero <brian.guerrero0210@gmail.com>
2023-02-22 14:49:24 -0500

    FINISHED YIPPEE

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -13,7 +13,7 @@ public class SpawnObject : MonoBehaviour
 
     public void Spawn()
     {
-        Instantiate(originalObject, spawnPosition.position, spawnPosition.rotation);
+        Instantiate(originalObject, spawnPosition.position, originalObject.transform.rotation);
     }
 
     private void OnValidate()
