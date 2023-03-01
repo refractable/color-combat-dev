a46314c260be8df2418654d8ed27f1729f24b285
Brian Guerrero <brian.guerrero0210@gmail.com>
2023-03-01 14:57:13 -0500

    Adjusted testing scene and projectile speed

--- Assets/Prefabs/Projectile.cs
+++ Assets/Prefabs/Projectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private float speed = 20;
+    private float speed = 30;
 
     // Start is called before the first frame update
     void Start()
