925aee355a5d380c726e593ae069210b138a5696
MarcoQuill <aidenb562303@gmail.com>
2023-04-13 13:54:51 -0400

    Added ENEMY
    Aye, that deserves a drink

---
+++ Assets/Scripts/EnemySystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySystem : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 4.0f;
+    Rigidbody rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Make enemy follow player
+        Vector3 pos=Vector3.MoveTowards(transform.position, target.position,speed*Time.fixedDeltaTime);
+        transform.LookAt(target);
+    }
+}
