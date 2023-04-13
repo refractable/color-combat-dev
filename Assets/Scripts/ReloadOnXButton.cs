480549e0ef357fdbd09e63983a9f0da735214e94
MarcoQuill <61989685+hr-oss@users.noreply.github.com>
2023-04-13 15:01:23 -0400

    Reload scened and fix buttons

---
+++ Assets/Scripts/ReloadOnXButton.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class ReloadOnXButton : MonoBehaviour
+{
+    private InputDevice rightController;
+
+    void Start()
+    {
+        // Find the right controller
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+        if (devices.Count > 0)
+        {
+            rightController = devices[0];
+        }
+    }
+
+    void Update()
+    {
+        // Check if the X button is pressed down on the right controller
+        if (rightController != null && rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPressed) && isPressed)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
