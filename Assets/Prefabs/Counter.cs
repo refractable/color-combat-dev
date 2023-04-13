ebf9924d6578b86a955e931137f0ee49fe9da034
MarcoQuill <aidenb562303@gmail.com>
2023-04-13 13:58:59 -0400

    Clocc
    Co-Authored-By: Erixkg <89102276+Erixkg@users.noreply.github.com>
    Co-Authored-By: LeoneTheCoder <117040847+LeoneTheCoder@users.noreply.github.com>
    Co-Authored-By: MarcoQuill <89092508+MarcoQuill@users.noreply.github.com>
    Co-Authored-By: joebambe <92176954+joebambe@users.noreply.github.com>
    Co-Authored-By: Hikora-Aulari <95308950+Hikora-Aulari@users.noreply.github.com>
    Co-Authored-By: refractable <89110801+refractable@users.noreply.github.com>

---
+++ Assets/Prefabs/Counter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Counter : MonoBehaviour
+{
+    public int minutes = 3; // set the number of minutes here
+
+    private float timer;
+    public TextMeshProUGUI textMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = minutes * 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        int minutesLeft = Mathf.FloorToInt(timer / 60f);
+        int secondsLeft = Mathf.FloorToInt(timer % 60f);
+
+        textMesh.text = string.Format("{0:0}:{1:00}", minutesLeft, secondsLeft);
+
+        if (timer <= 0f)
+        {
+            // call your function here
+            DoSomething();
+
+            textMesh.text = "DONE!";
+        }
+    }
+
+    void DoSomething()
+    {
+        // your function code goes here
+        Debug.Log("Done!");
+    }
+}
