630efcb3775c1baf36830b29076f70bf1472f221
MarcoQuill <aidenb562303@gmail.com>
2023-04-14 08:17:08 -0400

    AI WOO

--- Assets/Scripts/EnemySystem.cs
+++ Assets/Scripts/EnemySystem.cs
@@ -8,10 +8,20 @@ public class EnemySystem : MonoBehaviour
     public Transform target;
     public float speed = 4.0f;
     Rigidbody rig;
+    public GameObject enemyprojectile;
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody>();
+        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        Physics.IgnoreCollision(enemy.GetComponent<Collider>(), GetComponent<Collider>());
+        InvokeRepeating("SpawnProjectile", 1.0f, 2.0f);
+    }
+
+    void SpawnProjectile()
+    {
+        Instantiate(enemyprojectile, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
@@ -20,5 +30,13 @@ public class EnemySystem : MonoBehaviour
         // Make enemy follow player
         Vector3 pos=Vector3.MoveTowards(transform.position, target.position,speed*Time.fixedDeltaTime);
         transform.LookAt(target);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "Projectile")
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    private float speed = 20;
+    public float zRange = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        Physics.IgnoreCollision(enemy.GetComponent<Collider>(), GetComponent<Collider>());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Sets the projectile at a constant speed
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+        if (col.gameObject.tag == "Border")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
