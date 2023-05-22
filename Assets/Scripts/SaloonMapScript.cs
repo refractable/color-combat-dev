c85968930a6e2e2fb291e9452823669f11a499e2
Erick Guerra <guer512u02@ITACADEMY.LOCAL>
2023-05-22 15:27:53 -0400

    added map nav

---
+++ Assets/Scripts/SaloonMapScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaloonMapScript : MonoBehaviour
+{
+    public int level;
+    public GameObject enemy;
+    public GameObject player;
+
+    public int enemyCount;
+    public int playerCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        playerCheck = GameObject.FindGameObjectsWithTag("Player").Length;
+        if (enemyCount == 0)
+        {
+            levelUp(level);
+        }
+    }
+
+    void levelUp(int level1)
+    {
+        level += 1;
+
+        if (level == 1)
+        {
+            Instantiate(enemy, new Vector3(-82.26f, 0.49f, 3.63f), enemy.transform.rotation);
+        }
+        if (level == 2)
+        {
+            Instantiate(enemy, new Vector3(66.6f, 0.49f, 7.87f), enemy.transform.rotation);
+            Instantiate(enemy, new Vector3(66.6f, 0.49f, 12.14f), enemy.transform.rotation);
+        }
+        if (level == 3)
+        {
+            Instantiate(enemy, new Vector3(-1.2f, 2.83f, -59.69f), enemy.transform.rotation);
+        }
+        if (level == 4)
+        {
+            Instantiate(enemy, new Vector3(66.6f, 0.49f, 7.87f), enemy.transform.rotation);
+            Instantiate(enemy, new Vector3(-82.26f, 0.49f, 3.63f), enemy.transform.rotation);
+        }
+        if (level >= 5)
+        {
+            Instantiate(enemy, new Vector3(-1.2f, 2.83f, -59.69f), enemy.transform.rotation);
+            Instantiate(enemy, new Vector3(-82.26f, 0.49f, 3.63f), enemy.transform.rotation);
+        }
+    }
+}
+
